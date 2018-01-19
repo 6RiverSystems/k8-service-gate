b9d7b71b1d6ce5f41438cb6993ef26a3768382d1
Joseph Hughes <jjhughes57@gmail.com>
2018-01-19 10:47:41 -0500

    fix: touch pid for nginx bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/ash
 # shellcheck shell=dash
 
-RESOLVER_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
+RESOLVER_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'e)
 sed -i "s/\$RESOLVER_IP/${RESOLVER_IP}/g" /etc/nginx/conf.d/default.conf
 sed -i "s/\$SERVICE_NAME/${SERVICE_NAME}/g" /etc/nginx/conf.d/default.conf
 
+mkdir -p /run/nginx
+touch /run/nginx/nginx.pid
 
 /bin/oauth2_proxy -set-xauthrequest --email-domain=${EMAIL_DOMAIN} -config=/etc/oauth2_proxy.cfg &
 /usr/sbin/nginx -g 'daemon off;'
