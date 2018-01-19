335ab9ad2593fe363dc85d6f623637512ce7c2df
Joseph Hughes <jjhughes57@gmail.com>
2018-01-19 10:50:54 -0500

    fix: add upstream

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ sed -i "s/\$SERVICE_NAME/${SERVICE_NAME}/g" /etc/nginx/conf.d/default.conf
 mkdir -p /run/nginx
 touch /run/nginx/nginx.pid
 
-/bin/oauth2_proxy -set-xauthrequest --email-domain=${EMAIL_DOMAIN} -config=/etc/oauth2_proxy.cfg &
+/bin/oauth2_proxy -set-xauthrequest --upstream=http://127.0.0.1:80/ --email-domain=${EMAIL_DOMAIN} -config=/etc/oauth2_proxy.cfg &
 /usr/sbin/nginx -g 'daemon off;'
